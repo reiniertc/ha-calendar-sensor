e1efaa1b6f9de677e80ce5bfdc915feebb47d616
reiniertc <reinier@taudin-chabot.nl>
2025-11-20 07:22:06 +0100

    Update const.py

--- custom_components/ha_calendar_sensor/const.py
+++ custom_components/ha_calendar_sensor/const.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+from homeassistant.const import CONF_SCAN_INTERVAL
+
 DOMAIN = "ha_calendar_sensor"
 
 CONF_CALENDAR_ENTITY = "calendar_entity"
@@ -6,4 +9,8 @@ CONF_MAX_EVENTS = "max_events"
 
 DEFAULT_DAYS_AHEAD = 2
 DEFAULT_MAX_EVENTS = 12
-UPDATE_INTERVAL_MINUTES = 15
+
+# scan-interval in minuten (te gebruiken in options flow)
+DEFAULT_SCAN_INTERVAL = 15
+MIN_SCAN_INTERVAL = 1
+MAX_SCAN_INTERVAL = 120
