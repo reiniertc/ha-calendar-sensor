04ed18219fa0f1fe54b235329362581aeb86c3ed
reiniertc <reinier@taudin-chabot.nl>
2025-11-20 07:24:59 +0100

    Update config_flow.py

--- custom_components/ha_calendar_sensor/config_flow.py
+++ custom_components/ha_calendar_sensor/config_flow.py
@@ -6,6 +6,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.helpers.selector import selector
+from homeassistant.const import CONF_SCAN_INTERVAL
 
 from .const import (
     DOMAIN,
@@ -14,6 +15,9 @@ from .const import (
     CONF_MAX_EVENTS,
     DEFAULT_DAYS_AHEAD,
     DEFAULT_MAX_EVENTS,
+    DEFAULT_SCAN_INTERVAL,
+    MIN_SCAN_INTERVAL,
+    MAX_SCAN_INTERVAL,
 )
 
 
@@ -55,3 +59,35 @@ class HaCalendarSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+    @staticmethod
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry):
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input: dict[str, Any] | None = None):
+        if user_input is not None:
+            # data komt in entry.options terecht
+            return self.async_create_entry(title="", data=user_input)
+
+        current = self.config_entry.options.get(
+            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+        )
+
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_SCAN_INTERVAL,
+                    default=current,
+                ): vol.All(
+                    int,
+                    vol.Range(min=MIN_SCAN_INTERVAL, max=MAX_SCAN_INTERVAL),
+                ),
+            }
+        )
+
+        return self.async_show_form(step_id="init", data_schema=schema)
